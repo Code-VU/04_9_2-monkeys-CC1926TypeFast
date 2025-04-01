61ec93a016993854cc4c925ec66487beb74caf3d
Chris Cooper <Christopher.Cooper@vu.com>
2025-04-01 18:24:30 -0500

    Assigned values to specific string response

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -4,6 +4,15 @@ def calculateTime():
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
 
+    y = 1
+    n = 2
+
+    if monkey_one == monkey_two:
+        print("Uh Oh! We're in trouble!")
+    
+    elif monkey_one != monkey_two:
+        print("Yay! We're going to have a good day!")
+
     # end assignment
 
 
